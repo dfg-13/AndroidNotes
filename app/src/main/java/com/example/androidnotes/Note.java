49f621da253828b08cb37d845b239bf4f2edad6a
David Galicia <dgalicia@fourier.cs.iit.edu>
2022-09-26 21:04:55 -0500

    update: Note class

--- app/src/main/java/com/example/androidnotes/Note.java
+++ app/src/main/java/com/example/androidnotes/Note.java
@@ -1,16 +1,29 @@
 package com.example.androidnotes;
 
+import android.util.JsonWriter;
 import androidx.annotation.NonNull;
+import java.io.IOException;
+import java.io.Serializable;
+import java.io.StringWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class Note {
+public class Note implements Serializable {
     private String title;
     private String noteText;
-    private String lastSave;
+    private String dateTime;
 
-    public Note(String title, String noteText, String lastSave){
-        this.title = title;
-        this.noteText = noteText;
-        this.lastSave = lastSave;
+    public Note(String title, String noteText){
+        setTitle(title);
+        setNoteText(noteText);
+        setDateTime();
+    }
+
+    public Note(String title, String noteText, String dateTime){
+        setTitle(title);
+        setNoteText(noteText);
+        setDateTime(dateTime);
     }
 
     public String getTitle() {
@@ -29,21 +42,47 @@ public class Note {
         this.noteText = noteText;
     }
 
-    public String getLastSave() {
-        return lastSave;
+    public String getDateTime() {
+        return dateTime;
     }
 
-    public void setLastSave(String lastSave) {
-        this.lastSave = lastSave;
+    public void setDateTime() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        this.dateTime = dateFormat.format(date);
+    }
+
+    public void setDateTime(String dateTime){
+        this.dateTime = dateTime;
     }
 
     @NonNull
+    public String toJSON(){
+        try{
+            StringWriter sw = new StringWriter();
+            JsonWriter jw = new JsonWriter(sw);
+            jw.setIndent("  ");
+            jw.beginObject();
+
+            jw.name("title").value(getTitle());
+            jw.name("noteText").value(getNoteText());
+            jw.name("dateTime").value(getDateTime());
+
+            jw.endObject();
+            jw.close();
+            return sw.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
     @Override
     public String toString() {
         return "Note{" +
                 "title='" + title + '\'' +
                 ", noteText='" + noteText + '\'' +
-                ", lastSave='" + lastSave + '\'' +
+                ", lastSave='" + dateTime + '\'' +
                 '}';
     }
 }
