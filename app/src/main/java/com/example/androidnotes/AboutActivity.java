111ef61766e7a24552e0cd9bc009fcbfd01034c4
David Galicia <dgalicia@fourier.cs.iit.edu>
2022-10-02 00:17:36 -0500

    Update: title bar hidden

--- app/src/main/java/com/example/androidnotes/AboutActivity.java
+++ app/src/main/java/com/example/androidnotes/AboutActivity.java
@@ -1,5 +1,6 @@
 package com.example.androidnotes;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,7 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 }
